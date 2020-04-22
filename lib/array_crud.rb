28c1f517c9cb18933eb65522cda72cf7fee10845
Michael Jones <djyoungjones@gmail.com>
2020-04-22 21:26:38 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -48,7 +48,7 @@ def retrieve_last_element_from_array(array)
 end
 
 def update_element_from_index(array, index_number, element)
-  update_element = ["array", "index_number", "element", "wow"]
+  update_element = ["array", "index_number", "element", "arrays", "wow"]
   update_element[4] = "totally"
 
 end
